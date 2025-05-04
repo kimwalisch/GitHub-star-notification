245affc7937a9ebf6d26b4aca8acfaee1b8c2f3d
Kim Walisch <kim.walisch@gmail.com>
2025-05-04 16:41:06 +0200

    Update

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo dnf update -y
 sudo dnf install -y cronie python3-pip
 sudo systemctl start crond
 sudo -u ec2-user bash -c '
-  /usr/bin/python3 -m pip install requests urllib3 boto3
+  /usr/bin/python3 -m pip install requests urllib3
   curl -s https://raw.githubusercontent.com/kimwalisch/GitHub-star-notification/refs/heads/main/notify.py -o /home/ec2-user/notify.py
   curl -s https://raw.githubusercontent.com/kimwalisch/GitHub-star-notification/refs/heads/main/run.sh -o /home/ec2-user/run.sh
   chmod +x /home/ec2-user/run.sh
