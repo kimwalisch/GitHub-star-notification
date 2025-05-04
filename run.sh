18d191d8aa31fd57c14b3dc1f81e57cc3cf66860
Kim Walisch <kim.walisch@gmail.com>
2025-05-04 16:41:57 +0200

    Revert

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 EMAIL_ADDRESS=$(aws ssm get-parameter --name "EMAIL_ADDRESS" --with-decryption --query "Parameter.Value" --output text --region us-east-2)
+EMAIL_PASSWORD=$(aws ssm get-parameter --name "EMAIL_PASSWORD" --with-decryption --query "Parameter.Value" --output text --region us-east-2)
 GITHUB_TOKEN=$(aws ssm get-parameter --name "GITHUB_TOKEN" --with-decryption --query "Parameter.Value" --output text --region us-east-2)
 export EMAIL_ADDRESS="$EMAIL_ADDRESS"
+export EMAIL_PASSWORD="$EMAIL_PASSWORD"
 export GITHUB_TOKEN="$GITHUB_TOKEN"
 /usr/bin/python3 /home/ec2-user/notify.py
